2ee8994e41a8f40f9caee9750ff2ab93f0ed7e02
Dikki AP <dikki.agung@epson.co.id>
2025-11-05 10:35:05 +0700

    Initial

---
+++ src/main/java/dikki_dev/learn_spring_data_jpa/LearnSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package dikki_dev.learn_spring_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dikki_dev/learn_spring_data_jpa/LearnSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package dikki_dev.learn_spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
