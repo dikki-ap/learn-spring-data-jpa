9341550a46ab5be347af1b6219742c4125493580
Dikki AP <dikki.1710@gmail.com>
2025-11-06 20:03:28 +0700

    22. 'Auditrail' Timestamp and '@EnableJpaAuditing', '@CreatedDate', '@LastModifiedDate' Annotation

--- src/main/java/dikki_dev/learn_spring_data_jpa/LearnSpringDataJpaApplication.java
+++ src/main/java/dikki_dev/learn_spring_data_jpa/LearnSpringDataJpaApplication.java
@@ -2,8 +2,10 @@ package dikki_dev.learn_spring_data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Enable fungsi JPA Auditing untuk mengaktifkan fitur "timestamp" "created_date" dan "last_modified_date"
 public class LearnSpringDataJpaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/dikki_dev/learn_spring_data_jpa/entities/Category.java
+++ src/main/java/dikki_dev/learn_spring_data_jpa/entities/Category.java
@@ -5,7 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.Instant;
 import java.util.List;
 
 @Getter
@@ -23,4 +26,14 @@ public class Category {
 
     @OneToMany(mappedBy = "category")
     private List<Product> products;
+
+    // Menambahkan "Auditrail" timestamp
+    @CreatedDate // Wajib enable "@EnableJpaAuditing"
+    @Column(name = "created_date")
+    private Instant createdDate;
+
+    // Menambahkan "Auditrail" timestamp
+    @LastModifiedDate // Wajib enable "@EnableJpaAuditing"
+    @Column(name = "last_modified_date")
+    private Instant lastModifiedDate;
 }
