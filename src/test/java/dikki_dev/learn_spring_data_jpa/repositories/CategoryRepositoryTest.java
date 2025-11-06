db6266befc4b34022fdf12b5872f660790669038
Dikki AP <dikki.1710@gmail.com>
2025-11-06 09:44:37 +0700

    4. CRUDRepository and ListCRUDRepository

---
+++ src/test/java/dikki_dev/learn_spring_data_jpa/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,35 @@
+package dikki_dev.learn_spring_data_jpa.repositories;
+
+import dikki_dev.learn_spring_data_jpa.entities.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void testCreateCategory(){
+        Category category = new Category();
+        category.setName("Electronic"); // Id tidak perlu dibuat karena auto generated
+
+        // Method "save(entity)" berfungsi untuk CREATE dan UPDATE
+        categoryRepository.save(category); // Langsung save ke Database
+        Assertions.assertNotNull(category.getId());
+    }
+
+    @Test
+    void testUpdateCategory(){
+        Category category = categoryRepository.findById(1L).orElse(null); // GET ID 1, jika tidak dapat return null
+        category.setName("Small Electronic"); // Ubah existing "name" data nya
+
+        // Method "save(entity)" berfungsi untuk CREATE dan UPDATE
+        categoryRepository.save(category); // Update, dan save ke Database
+        Assertions.assertNotNull(category.getId());
+        Assertions.assertEquals("Small Electronic", category.getName());
+    }
+}
