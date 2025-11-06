e694ddd6e8e9dc5ad69a0ddac212540dc08a56ed
Dikki AP <dikki.1710@gmail.com>
2025-11-06 10:06:46 +0700

    6. '@Transactional' Propagation Property

--- src/main/java/dikki_dev/learn_spring_data_jpa/services/CategoryService.java
+++ src/main/java/dikki_dev/learn_spring_data_jpa/services/CategoryService.java
@@ -22,6 +22,33 @@ public class CategoryService {
         - Misalnya ini dibuat di CategoryService.create(), harus dipanggil di luar object CategoryService agar @Transactional berjalan
         - Jika dipanggil di object yang sama, tidak akan berfungsi, karena @Transactional itu dibungkus oleh AOP seperti proxy
      */
+
+    /*
+        -- @Transactional Propagation --
+        - Berfungsi jika ServiceA menggunakan @Transactional, dan di dalam methodnya memanggil ServiceB yang menggunakan @Transactional juga
+        - Jadi seperti Nested @Transactional, ada @Transactional di dalam @Transactional yang lain
+
+        1. REQUIRED        : Default. Gunakan transaksi yang sedang berjalan, atau buat baru jika tidak ada.
+                        -> Cocok untuk use case umum.
+
+        2. REQUIRES_NEW    : Selalu buat transaksi baru. Suspend transaksi sebelumnya.
+                            -> Untuk operasi terpisah seperti email, audit log, dsb.
+
+        3. MANDATORY       : Harus dipanggil dalam transaksi. Kalau tidak ada transaksi, throw Exception.
+                            -> Pastikan konteks atomic sudah disiapkan di outer method.
+
+        4. SUPPORTS        : Ikut transaksi kalau ada, kalau tidak ya jalan tanpa transaksi.
+                            -> Untuk method yang fleksibel, seperti query read-only.
+
+        5. NOT_SUPPORTED   : Suspend transaksi jika ada, dan jalankan non-transactional.
+                            -> Untuk proses yang memakan waktu atau tidak perlu transactional.
+
+        6. NEVER           : Tidak boleh ada transaksi. Kalau ada transaksi berjalan, throw Exception.
+                            -> Untuk logic yang tidak boleh di-rollback.
+
+        7. NESTED          : Seperti REQUIRED, tapi buat savepoint. Inner rollback tidak membatalkan outer.
+                            -> Bagus untuk proses batch atau partial rollback.
+     */
     @Transactional
     public void create(){
         for (int i = 0; i < 5; i++) {
